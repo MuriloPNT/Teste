cbf985b132a0904259f63aa8499ae2c3e7636d4a
MuriloPNT <pinto.murilo13@escola.pr.gov.br>
2025-06-02 08:55:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'agrônomia',
+    'qual o nome científico do capim estrela africana?',
+    'o nome científico é Cynodon nlemfuensis Vanderyst'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'veterinária',
+    'O amarelão é formado por quantos parasitas?',
+    'são dois pequenos parasitas chamados Ancylostoma duodenale e Necator americanus'
+)
